b25707ff979b236f58c8a39c1b48a27d69dfc223
Mogul <mogul27442@gmail.com>
2022-09-25 20:34:03 +0100

    Now off tutorial

--- todo-next-js/components/FilterButton.js
+++ todo-next-js/components/FilterButton.js
@@ -3,12 +3,14 @@ import React from "react";
 export default function FilterButton(props){
 
     return(
-    <button type="button" className = "btn toggle-btn" 
-    aria-pressed="true"> 
+    <button type="button" className="btn toggle-btn" 
+    aria-pressed={props.isPressed} onClick={() => props.setFilter(props.name)}> 
 
     <span className="visually-hidden">
-        All
+        Show
     </span>
+    <span> {props.name}</span>
+    <span className="visually-hidden"> tasks</span>
     </button>
     )
 }
--- todo-next-js/pages/today.js
+++ todo-next-js/pages/today.js
@@ -6,17 +6,37 @@ import Form from "../components/Form";
 import FilterButton from "../components/FilterButton";
 import React, { useState } from "react";
 import { nanoid } from "nanoid";
+import styles from "../styles/eachDay.css"
 
+
+const FILTER_MAP = {All: () => true, 
+                    Active: (task) => !task.completed,
+                    Completed: (task) => task.completed};
+
+const FILTER_NAMES = Object.keys(FILTER_MAP);
 
 export default function TodayList() {
     
     const [tasks, setTasks] = useState(require('../today_tasks.json'))
-    const taskList = tasks.map((task) => <Todo key={task.id}  id={task.id}
-    name={task.name} completed={task.completed} />);
+
+    const [filter, setFilter] = useState('All');
+
+    const taskList = tasks.filter(FILTER_MAP[filter]).map((task) =>
+    (<Todo key={task.id}  id={task.id}
+    name={task.name} completed={task.completed} 
+    toggleTaskCompleted={toggleTaskCompleted} 
+    deleteTask={deleteTask}
+    editTask={editTask}/>));
+
+    const filterList = FILTER_NAMES.map((name) => 
+    (<FilterButton key={name}
+                   name={name}
+                   isPressed={name==filter}
+                   setFilter={setFilter}/>));
 
     // Displaying the number of tasks or task left
     const taskNoun = taskList.length !== 1 ? 'tasks' : 'task';
-    const headingText = "${taskList.length} ${tasksNoun} remaining";
+    const headingText = `${taskList.length} ${taskNoun} remaining`;
 
     function addTask(name) {
         const newTask = { id: nanoid(), name, completed: false };
@@ -24,6 +44,32 @@ export default function TodayList() {
         // into a Task structure
         setTasks([...tasks, newTask])
         // Write data structure to JSON
+    }
+
+    function toggleTaskCompleted(id) {
+        const updatedTasks = tasks.map((task) => {
+            if (id == task.id) {
+                // Overwrites completed value with the opposite value
+                return {...task, completed: !task.completed}
+            }
+            return task;
+        });
+        setTasks(updatedTasks)
+    }
+
+    function deleteTask(id) {
+        const remainingTasks = tasks.filter((task) => id !== task.id);
+        setTasks(remainingTasks)
+    }
+
+    function editTask(id, newName) {
+        const editedTaskList = tasks.map((task) => {
+            if (id == task.id){
+                return {...task, name: newName}
+            }
+            return task;
+        });
+        setTasks(editedTaskList);
     }
 
     return (
@@ -41,11 +87,7 @@ export default function TodayList() {
 
     {/* These are the buttons for filtering tasks */}
     <div className="filters btn-group stack-exception">
-    
-    <FilterButton />
-    <FilterButton />
-    <FilterButton />
-
+    {filterList}
     </div>
 
     <h2 id="list-heading">
